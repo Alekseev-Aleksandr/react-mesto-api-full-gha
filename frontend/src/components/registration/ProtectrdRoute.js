1fdd19fe353575c10cbbcae05dd61bb7ae86cbf3
Alekseev Aleksandr <Alekseev.7aa@yandex.ru>
2023-07-27 04:16:36 +0300

    fix:import react

--- frontend/src/components/registration/ProtectrdRoute.js
+++ frontend/src/components/registration/ProtectrdRoute.js
@@ -1,4 +1,4 @@
-import Navigate from "react-router-dom";
+import React, { Navigate } from "react-router-dom";
 
 export default function ProtectedRoute({ element: Component, ...props }) {
     return (
